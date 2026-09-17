02be269d13c09d72d3b5cf0fedebd3cf3bff7b74
Imane Rachidi <imanrachidi003@gmail.com>
2026-09-17 10:49:42 +0100

    modificati

--- challenge1.js
+++ challenge1.js
@@ -1,1 +1,15 @@
-console.log("hello javascript");
+const prompt = require('prompt-sync')(); 
+
+let nom = prompt("Quel est votre nom ?");
+let prenom = prompt("Quel est votre prénom ?");
+let age = prompt("Quel est votre âge ?");
+let sexe = prompt("Quel est votre sexe (M/F) ?");
+let email = prompt("Quelle est votre adresse e-mail ?");
+
+// 2. Affichage du résultat
+console.log(`Voici vos informations :
+- Nom : ${nom}
+- Prénom : ${prenom}
+- Âge : ${age} ans
+- Sexe : ${sexe}
+- E-mail : ${email}`);
