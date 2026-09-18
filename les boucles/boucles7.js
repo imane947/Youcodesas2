930537be68893f82b96b5f0d5af43a819bee3f4d
Imane Rachidi <imanrachidi003@gmail.com>
2026-09-18 13:00:13 +0100

    math

--- les boucles/boucles7.js
+++ les boucles/boucles7.js
@@ -1,1 +1,9 @@
 const prompt = require('prompt-sync')();
+
+let n = parseInt(prompt("entrez le nombre de entier :"));
+let i = 0;
+while (n > 0) {
+    i = i * 10 + n % 10;
+    n = Math.floor (n / 10) ;
+}
+console.log(i);
